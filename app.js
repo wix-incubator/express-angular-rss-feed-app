fe5bca184b9021ee70a6515faba2b80f2127dace
Lior Shefer <lior.shefer@wix.com>
2013-11-23 14:57:47 +0200

    server - express.js backend with mongodb to serve the widget and settings pages and store the app state

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var Authentication = require('./authentication.js');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+function authenticate(req, res, next) {
+    var authentication = new Authentication();
+    authentication.authenticate(req, res, next);
+}
+
+app.get('/widget', authenticate, routes.widget);
+app.get('/settings', authenticate, routes.settings);
+app.post('/app/settingsupdate', authenticate, routes.settingsupdate);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,43 @@
+
+var config = require('konphyg')(__dirname + '../../config');
+var dbConfig = config('db');
+var db = require("mongojs").connect(dbConfig.url, dbConfig.collections);
+
+/*
+ * GET widget.
+ */
+exports.widget = function(req, res){
+    getSettings(req, function(data){
+        res.render('widget', { settings: data });
+    })
+};
+
+/*
+ * GET settings.
+ */
+exports.settings = function(req, res){
+    getSettings(req, function(data){
+        res.render('settings', { settings: data });
+    })
+};
+
+/*
+ * POST settings update.
+ */
+exports.settingsupdate = function(req, res){
+    db.settings.update({key: req.key}, { $set: { settings: req.body.settings }},  { upsert: true }, function(err, saved) {
+        res.end();
+    });
+};
+
+function getSettings(req, callback){
+    db.settings.findOne({key: req.key}, function(err, result) {
+        if( err || !result) {
+            callback({});
+        }
+        else{
+            console.log(result.settings);
+            callback(result.settings);
+        };
+    });
+}
